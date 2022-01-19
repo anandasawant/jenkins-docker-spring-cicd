ed264cb7c538e710199823da7cf193fb5815bb6c
ananda.sawant <ananda.sawant@collabera.com>
2022-01-19 10:59:43 +0530

    Controller

---
+++ src/test/java/com/ani/cicd/cicdjenkinsdemo/cicdjenkinsdemo.java
@@ -0,0 +1,18 @@
+package com.ani.cicd.cicdjenkinsdemo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class cicdjenkinsdemo {
+
+    @Autowired
+    private SmsController controller;
+
+    @Test
+    void contextLoads() {
+        Assertions.assertNotNull(controller);
+    }
+}
